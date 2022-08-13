ca4a2a5071796a036966aa5e1c1f9c4e41c15909
King <king@kings-air.lan>
2022-08-12 21:50:45 -0700

    first pass local authentication

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,7 +1,12 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState } from 'react';
+
 import { StyleSheet, View, Text, Button } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import type { StackNavigationProp } from '@react-navigation/stack';
+
+import * as LocalAuthentication from 'expo-local-authentication';
+
+
 
 import {RootStackParamList} from '../navigation/types'; 
 
@@ -13,6 +18,13 @@ import {
 } from "@expo-google-fonts/roboto-mono";
 
 import { Creepster_400Regular } from "@expo-google-fonts/creepster";
+
+enum EResult {
+  CANCELLED = 'CANCELLED',
+  DISABLED = 'DISABLED',
+  ERROR = 'ERROR',
+  SUCCESS = 'SUCCESS',
+}
 
 
 type welcomeScreenProp = StackNavigationProp<RootStackParamList, 'Welcome'>;
@@ -30,6 +42,96 @@ const WelcomeScreen = () => {
     Creepster_400Regular,
   });
 
+  const [facialRecognitionAvailable, setFacialRecognitionAvailable] = React.useState(false);
+  const [fingerprintAvailable, setFingerprintAvailable] = React.useState(false);
+  const [irisAvailable, setIrisAvailable] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [result, setResult] = React.useState<EResult>();
+
+  const checkSupportedAuthentication = async () => {
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    if (types && types.length) {
+      setFacialRecognitionAvailable(types.includes(LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION));
+      setFingerprintAvailable(types.includes(LocalAuthentication.AuthenticationType.FINGERPRINT));
+      setIrisAvailable(types.includes(LocalAuthentication.AuthenticationType.IRIS));
+    }
+  };
+
+ 
+
+  const authenticate = async () => {
+    if (loading) {
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const results = await LocalAuthentication.authenticateAsync();
+
+      if (results.success) {
+        setResult(EResult.SUCCESS);
+      } else if (results.error === 'unknown') {
+        setResult(EResult.DISABLED);
+      } else if (
+        results.error === 'user_cancel' ||
+        results.error === 'system_cancel' ||
+        results.error === 'app_cancel'
+      ) {
+        setResult(EResult.CANCELLED);
+      }
+    } catch (error) {
+      setResult(EResult.ERROR);
+    }
+
+    setLoading(false);
+  };
+
+  React.useEffect(() => {
+    checkSupportedAuthentication();
+  }, []);
+
+  let resultMessage;
+  switch (result) {
+    case EResult.CANCELLED:
+      resultMessage = 'Authentication process has been cancelled';
+      break;
+    case EResult.DISABLED:
+      resultMessage = 'Biometric authentication has been disabled';
+      break;
+    case EResult.ERROR:
+      resultMessage = 'There was an error in authentication';
+      break;
+    case EResult.SUCCESS:
+      resultMessage = 'Successfully authenticated';
+      break;
+    default:
+      resultMessage = '';
+      break;
+  }
+
+  let description;
+  if (facialRecognitionAvailable && fingerprintAvailable && irisAvailable) {
+    description = 'Authenticate with Face ID, touch ID or iris ID';
+  } else if (facialRecognitionAvailable && fingerprintAvailable) {
+    description = 'Authenticate with Face ID or touch ID';
+  } else if (facialRecognitionAvailable && irisAvailable) {
+    description = 'Authenticate with Face ID or iris ID';
+  } else if (fingerprintAvailable && irisAvailable) {
+    description = 'Authenticate with touch ID or iris ID';
+  } else if (facialRecognitionAvailable) {
+    description = 'Authenticate with Face ID';
+  } else if (fingerprintAvailable) {
+    description = 'Authenticate with Touch ID ';
+  } else if (irisAvailable) {
+    description = 'Authenticate with iris ID';
+  } else {
+    description = 'Biometric authentication is not supported on this device.';
+  }
+
+
+
+
   if (!fontsLoaded) {
     return null;
   } else {
@@ -45,6 +147,13 @@ const WelcomeScreen = () => {
           <Text style={[styles.text, { fontSize: 28, paddingBottom: 40, fontFamily: "RobotoMono_500Medium" }]}>
          Who knows what will happen next? 
         </Text>
+        <Text>{description}. </Text>
+        {facialRecognitionAvailable || fingerprintAvailable || irisAvailable ? (
+          <Button title="Authenticate" onPress={ authenticate} />
+        ) : null
+        }
+        {resultMessage ? navigation.navigate('Main')  : null }
+        <Text>Similate a login by clicking the button. </Text>
         <Button title="Enter" onPress={() => navigation.navigate('Main')} />
     </View>
   );
