4612f06bc410e987a64c4e1ee8b4507a30563b60
“Eric <eric.hestenes@dotterpop.com>
2022-08-14 18:28:33 -0700

    fix contact detail for android

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 
 import { useRoute } from '@react-navigation/native';
 
@@ -8,16 +8,22 @@ import type { RouteProp } from '@react-navigation/native';
 
 type DetailsScreenRouteProp = RouteProp<ContactStackNavigatorParamList, 'Details'>;
 
+import { globalStyles } from '../styles/styles';
+
+
 const DetailScreen = () => {
   const route = useRoute<DetailsScreenRouteProp>();
   const { name, birthYear } = route.params;
 
   return (
-    <View style={{ flex: 1, backgroundColor: '#FBEEC6', paddingTop: 12, paddingHorizontal: 10 }}>
-      <Text style={{ fontSize: 18, paddingBottom: 24 }}>This is the Details screen.</Text>
-      <Text style={{ fontSize: 18, paddingBottom: 12 }}>Name: {name}</Text>
-      <Text style={{ fontSize: 18 }}>Birth Year: {birthYear}</Text>
-    </View>
+    <ScrollView style={globalStyles.droidSafeArea} contentContainerStyle={{ flexGrow: 1 }}>
+      <Text style={globalStyles.heading1}>Contact Details</Text>
+      <View style={{ flex: 1, backgroundColor: '#FBEEC6', paddingTop: 12, paddingHorizontal: 10 }}>
+        <Text style={{ fontSize: 18, paddingBottom: 24 }}>This is the Details screen.</Text>
+        <Text style={{ fontSize: 18, paddingBottom: 12 }}>Name: {name}</Text>
+        <Text style={{ fontSize: 18 }}>Birth Year: {birthYear}</Text>
+      </View>
+    </ScrollView>
   );
 };
 
